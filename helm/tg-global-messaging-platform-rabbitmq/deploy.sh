6bc975e7e7e7eb19ab5702c8968b6f10a5e4d55b
personal_shiva <me.shiva0059@gmail.com>
2025-04-28 14:24:48 -0500

    Add files via upload

---
+++ helm/tg-global-messaging-platform-rabbitmq/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+CURRENT_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+
+namespace=default
+
+function utils::get_helm_mode() {
+    if helm status $* >/dev/null 2>&1;  then
+        echo "Helm chart  '$1' already exist => upgrade mode" >&2
+        echo "upgrade"
+    else
+        echo "Helm chart '$1' doesnot exist => install mode" >&2
+        echo "install"
+    fi
+}
+
+mode=$(utils::get_helm_mode rabbitmq -n $namespace)
+
+kubectl create secret generic rabbitmq-definitions --from-file=load_definition.json=files/definitions.json -n $namespace
+helm $mode rabbitmq bitnami/rabbitmq -f values.yaml -n $namespace
+if [ $? -ne 0 ]; then 
+    echo "Failed to deploy Rabbit MQ"
+    exist 1
+fi
